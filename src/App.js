89d5624a5ff54d38159470cb968b281e25ddb619
Venus Ang <venus@users.noreply.github.com>
2022-10-31 15:27:35 -0700

    added state and ColorForm to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
 
 function App() {
-  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'];
+  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']);
 
   let colorMap = colors.map((color, i) => {
     return <ColorBlock color={color} key={`color-${i}`} />
   });
 
+  const addColor = (newColor) => {
+    setColors([...colors, newColor]);
+  }
+
   return (
     <div className="App">
       {colorMap}
+      <ColorForm addColor={addColor} />
     </div>
   );
 }
